55a448697355f23a893a1c8b4ed84d2c9b4667e8
AntoniaNivia <capic.2023118ISINF0012@aluno.ifpi.edu.br>
2024-08-29 11:02:07 -0300

    menu

--- js/entrar.js
+++ js/entrar.js
@@ -9,3 +9,8 @@ signUpButton.addEventListener('click', () => {
 signInButton.addEventListener('click', () => {
   container.classList.remove("right-panel-active");
 });
+
+function toggleNav(){
+  const nav =  document.querySelector('nav');
+  nav.classList.toggle('mobile');
+}
